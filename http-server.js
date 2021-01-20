dbb965d4718e464f066f0927595bce88b5a51e83
Bob <bob_w@qq.com>
2021-01-20 21:58:11 +0800

    创建一个服务

---
+++ http-server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((request, response) => {
+  const { method, url } = request;
+  if (method == 'GET' && url == '/') {
+    fs.readFile('./index.html', (err, data) => {
+      response.setHeader('Content-Type', 'text/html');
+      response.end(data);
+    });
+  } else if (method == 'GET' && url == '/users') {
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify([{ name: 'Tom', age: 20 }]));
+  }
+}).listen(3000);
