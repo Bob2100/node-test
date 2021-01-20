49025d1b71e52199ef9c969626bc233a8f395135
Bob <bob_w@qq.com>
2021-01-20 23:01:10 +0800

    解决preflight请求跨域问题

--- http-server.js
+++ http-server.js
@@ -8,9 +8,16 @@ http.createServer((request, response) => {
       response.setHeader('Content-Type', 'text/html');
       response.end(data);
     });
-  } else if (method == 'GET' && url == '/users') {
+  } else if ((method == 'GET' || method == 'POST') && url == '/users') {
     response.setHeader('Content-Type', 'application/json');
     response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
     response.end(JSON.stringify([{ name: 'Tom', age: 20 }]));
+  } else if (method == 'OPTIONS' && url == '/users') {
+    response.writeHead(200, {
+      "Access-Control-Allow-Origin": "http://localhost:3001",
+      "Access-Control-Allow-Headers": "X-Token,Content-Type",
+      "Access-Control-Allow-Methods": "GET,POST,PUT",
+    });
+    response.end();
   }
 }).listen(3000);
