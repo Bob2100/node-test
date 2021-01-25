d69593bd8646cd6735c5023f17f76348396299bd
Bob <bob_w@qq.com>
2021-01-25 13:37:28 +0800

    插入和查询数据

--- mysql.js
+++ mysql.js
@@ -4,14 +4,42 @@ const config = {
   host: 'localhost',
   user: 'root',
   password: '123456',
-  database: 'worth',
+  database: 'test',
   socketPath: '/tmp/mysql.sock'
 }
 
 const con = mysql.createConnection(config);
+
+const CREAT_SQL = `CREATE TABLE IF NOT EXISTS test  (
+  id int(0) NOT NULL AUTO_INCREMENT,
+  message varchar(45) NULL,
+  PRIMARY KEY (id)
+)`;
+const INSERT_SQL = `INSERT INTO test set ?`;
+const SELECT_SQL = `SELECT * FROM test`;
+
 con.connect(err => {
   if (err) {
     throw err;
   }
   console.log('连接成功');
+  con.query(CREAT_SQL, err => {
+    if (err) {
+      throw err;
+    }
+    console.log('表创建成功');
+    con.query(INSERT_SQL, { message: 'test5' }, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      console.log('插入数据成功：', result);
+      con.query(SELECT_SQL, (err, results) => {
+        if (err) {
+          throw err;
+        }
+        console.log('查询数据成功：', results);
+        con.end();
+      });
+    });
+  });
 });
